4d0b948a476db5b5abf851776fa2810aa8af76a4
daniel9091 <djguzman1@miuandes.cl>
2024-10-25 23:30:27 -0300

    implementacion de boon logout

--- hybrid-frontend/app/index.js
+++ hybrid-frontend/app/index.js
@@ -3,12 +3,22 @@ import { View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 // import { useNavigation } from '@react-navigation/native'; // Importa el hook
 import {useRouter } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 // El componente funcional MainIndex
 export default function MainIndex() {
   const router = useRouter()
   // const navigation = useNavigation(); // Usa el hook para obtener el objeto de navegación
+
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem('@user_id');
+      console.log('user_id borrado exitosamente');
+    } catch (error) {
+      console.error('Error al borrar user_id:', error);
+  }
+}
 
   return (
     <View style={styles.container}>
@@ -18,9 +28,14 @@ export default function MainIndex() {
       <Button mode="contained" onPress={() => router.push('/beers')}>
         Cervezas
       </Button>
+      <Button mode="contained" onPress={handleLogout}>
+        Logout
+      </Button>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
